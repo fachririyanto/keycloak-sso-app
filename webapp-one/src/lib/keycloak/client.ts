b6f06335ebe925ab3a8dc71737c36ba31c25825d
Fachri Riyanto <fachri@redpumpkin.ai>
2026-03-11 16:48:35 +0700

    update: move the code into branches

--- webapp-one/src/lib/keycloak/client.ts
+++
@@ -1,9 +0,0 @@
-import KeyCloak from "keycloak-js";
-
-import { KeycloakConfig } from "@/lib/config";
-
-export const keyCloak = new KeyCloak({
-    url: KeycloakConfig.url,
-    realm: KeycloakConfig.realm,
-    clientId: KeycloakConfig.clientId,
-});
